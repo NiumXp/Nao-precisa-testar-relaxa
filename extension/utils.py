1f85d175fe8622480225e49cb7bb11350e76a931
NiumXp <formigacomcaimbra@gmail.com>
2021-03-10 09:03:54 -0300

    fixado a definação de geração de valor em Cards

--- extension/utils.py
+++ extension/utils.py
@@ -3,13 +3,13 @@ from extension import emoji
 
 
 class Cards(enum.Enum):
+    def _generate_next_value_(name, *_):
+        name = name.capitalize() + "Card"
+        return emoji(name)
+
     RED = enum.auto()
     BLUE = enum.auto()
     GREEN = enum.auto()
     YELLOW = enum.auto()
     WHITE = enum.auto()
     PINK = enum.auto()
-
-    def _generate_next_value_(name, *_):
-        name = name.capitalize() + "Card"
-        return emoji(name)
