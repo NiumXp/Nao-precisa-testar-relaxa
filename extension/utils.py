fe180404eb57f2eb2fc742af4efd7b047b38d8dd
NiumXp <formigacomcaimbra@gmail.com>
2021-03-10 12:45:51 -0300

    adicioand documentação classe Cards

--- extension/utils.py
+++ extension/utils.py
@@ -3,6 +3,16 @@ from extension import emoji
 
 
 class Cards(enum.Enum):
+    """
+    Enumeração para as cartas do jogo.
+
+    O valor de cada atributo corresponde ao seu emoji - veja Nota.
+
+    Nota
+    ----
+    O valor do atributo é gerado pelo seu nome capitalizado e sufixado
+    com `Card`, depois é pego o valor utilizando `extension.utils.emoji`
+    """
     def _generate_next_value_(name, *_):
         name = name.capitalize() + "Card"
         return emoji(name)
