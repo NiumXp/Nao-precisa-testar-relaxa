2d98f3a8680d4783a1d5be37b1baee8870686587
NiumXp <formigacomcaimbra@gmail.com>
2021-03-14 06:46:07 -0300

    arrumado keyword_parameter errado

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from extension import CardBot
 # ambiente.
 bot_prefix = os.environ["BOT_PREFIX"]
 improved_bot_prefix = when_mentioned_or(bot_prefix)
-bot = CardBot(improved_bot_prefix, help=None)
+bot = CardBot(improved_bot_prefix, help_command=None)
 
 # Comando de help
 @bot.command()
